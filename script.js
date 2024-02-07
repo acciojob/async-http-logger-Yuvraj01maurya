bcc70a75631f6870d60cc83690fc27e0fe58e1f3
Yuvraj Maurya <yuvraj95166@gmail.com>
2024-02-07 22:15:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+// Make an asynchronous HTTP request
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {
+    // Check if the response is successful
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Parse the JSON body of the response
+    return response.json();
+  })
+  .then(data => {
+    // Log the response body to the console
+    console.log(data);
+  })
+  .catch(error => {
+    // Log any errors to the console
+    console.error('There was a problem with the fetch operation:', error);
+  });
